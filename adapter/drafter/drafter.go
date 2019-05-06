457eeebd7ce7efaa89d31f8f3ef6b5248531097e
Alif Rachmawadi <subosito@bukalapak.com>
2019-05-07 00:11:21 +0700

    updated c binding

--- adapter/drafter/drafter.go
+++ adapter/drafter/drafter.go
@@ -2,8 +2,8 @@ package drafter
 
 /*
 #cgo CFLAGS: -I"${SRCDIR}/ext/drafter/src/" -I"${SRCDIR}/ext/drafter/ext/snowcrash/src/"
-#cgo darwin LDFLAGS: -L"${SRCDIR}/ext/drafter/build/out/Release/" -ldrafter -lsos -lsnowcrash -lmarkdownparser -lsundown -lc++
-#cgo linux LDFLAGS: -L"${SRCDIR}/ext/drafter/build/out/Release/" -ldrafter -lsos -lsnowcrash -lmarkdownparser -lsundown -lstdc++
+#cgo darwin LDFLAGS: -L"${SRCDIR}/ext/drafter/build/out/Release/" -ldrafter -lsnowcrash -lmarkdownparser -lsundown -lc++
+#cgo linux LDFLAGS: -L"${SRCDIR}/ext/drafter/build/out/Release/" -ldrafter -lsnowcrash -lmarkdownparser -lsundown -lstdc++
 #include <stdlib.h>
 #include <stdio.h>
 #include "drafter.h"
@@ -28,10 +28,11 @@ func (e Engine) Parse(r io.Reader) ([]byte, error) {
 
 	cSource := C.CString(string(b))
 	cResult := &C.drafter_result{}
+	cOption := C.drafter_parse_options{requireBlueprintName: false}
 
-	code := int(C.drafter_parse_blueprint(cSource, &cResult))
+	code := int(C.drafter_parse_blueprint(cSource, &cResult, cOption))
 	if code != 0 {
-		return nil, fmt.Errorf("ParseBlueprint failed with code: %d", code)
+		return nil, fmt.Errorf("Parse failed with code: %d", code)
 	}
 
 	C.free(unsafe.Pointer(cSource))
@@ -46,7 +47,13 @@ func (e Engine) Validate(r io.Reader) ([]byte, error) {
 	}
 
 	cSource := C.CString(string(b))
-	cResult := C.drafter_check_blueprint(cSource)
+	cOption := C.drafter_parse_options{requireBlueprintName: false}
+	cResult := &C.drafter_result{}
+
+	code := int(C.drafter_check_blueprint(cSource, &cResult, cOption))
+	if code != 0 {
+		return nil, fmt.Errorf("Validate failed with code: %d", code)
+	}
 
 	C.free(unsafe.Pointer(cSource))
 
@@ -58,7 +65,7 @@ func (e Engine) Version() string {
 }
 
 func (e Engine) serialize(r *C.drafter_result) []byte {
-	options := C.drafter_options{sourcemap: false, format: C.DRAFTER_SERIALIZE_JSON}
+	options := C.drafter_serialize_options{sourcemap: false, format: C.DRAFTER_SERIALIZE_JSON}
 	cResult := C.drafter_serialize(r, options)
 	results := C.GoString(cResult)
 
